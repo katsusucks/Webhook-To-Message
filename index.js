691a3d4aa67cfc1f17576dd4eb5e3c1cfcf5b619
Kshitiz <84060730+KshitizTrashh@users.noreply.github.com>
2023-01-07 16:59:23 +0545

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fetch = require ("node-fetch")
 
 
 
-const webhookUrl = 'https://discord.com/api/webhooks/1061224336658272286/PottsTZm3G1nDe58M6rXp1mXuKzpdMsGXrBUbn0OZ6wPmNd13kfrZ5SkELa2-dJW_pKN';
-const message = 'Mickey Mouse, got the whole server laughing';
+const webhookUrl = 'URL'; //webhook url
+const message = 'MESSAGE'; //message
 getSend(webhookUrl, message);
 
 console.log('\033[32mCompleted the task');
@@ -12,7 +12,7 @@ console.log('\033[32mCompleted the task');
 
 function getSend(webhookUrl, message) {
   if (!webhookUrl) {
-    throw new Error('[Touya:] No Webhook was provided');
+    throw new Error('[Touya:] No Webhook was provided'); //can be changed
   }
   fetch(webhookUrl, {
     method: 'POST',
